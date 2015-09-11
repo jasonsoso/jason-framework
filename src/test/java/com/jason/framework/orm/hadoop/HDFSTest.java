0ceb63aeba755f64055ebeed7d27ca12af44d2a4
jasonsoso <648636045@qq.com>
2015-09-11 09:03:59 +0800

    hadoop2.6.0学习

--- src/test/java/com/jason/framework/orm/hadoop/HDFSTest.java
+++ src/test/java/com/jason/framework/orm/hadoop/HDFSTest.java
@@ -1,6 +1,7 @@
 package com.jason.framework.orm.hadoop;
 
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
@@ -16,14 +17,18 @@ import org.junit.Test;
 public class HDFSTest {
 
 	@Test
-	public void testHighlight() throws IOException {
+	public void test() throws IOException {
+		
 		String uri = "hdfs://hadoop-yarn.jasonsoso.com:8020";
 		Configuration config = new Configuration();
 		FileSystem fs = FileSystem.get(URI.create(uri), config);
+		
 		// 列出目录所有文件
 		FileStatus[] statuses = fs.listStatus(new Path("/data"));
 		for (FileStatus status : statuses) {
-			System.out.println(status);
+			String type = status.isDirectory()?"目录":"文件";
+			String name = status.getPath().getName();
+			System.out.println(status+": 类型："+type+" 文件名："+name);
 		}
 		// 创建新文件
 		FSDataOutputStream os = fs.create(new Path("/data/hdfs_test.txt"));
@@ -31,6 +36,19 @@ public class HDFSTest {
 		os.write("测试HDFS第二条\r\n".getBytes());
 		os.flush();
 		os.close();
+		
+		//上传文件
+		//FSDataOutputStream fSDataOutputStream = fs.create(new Path("/data/up.txt"));
+		//FileInputStream fileInputStream = new FileInputStream("up.txt");
+		//IOUtils.copyBytes(fileInputStream, fSDataOutputStream, 1024, true);
+		
+		//创建目录
+		fs.mkdirs(new Path("/new"));
+		
+		//删除目录
+		fs.delete(new Path("/new"),true);
+		
+		
 		// 读取文件
 		InputStream is = fs.open(new Path("/data/hdfs_test.txt"));
 		IOUtils.copyBytes(is, System.out, 1024, true);
